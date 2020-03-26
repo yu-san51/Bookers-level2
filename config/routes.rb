18ccb7b3a5dc8f32abc8d298872ce6804a63ba4d
yu-san51 <nekonyan.yuki51@gmail.com>
2020-03-26 08:11:35 +0000

    [Add] root.about

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  "/" => "home#index"
-  "/home/about" => "home#show"
+  root "home#index"
+  get "home/about" => "home#show"
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_26_042322) do
+ActiveRecord::Schema.define(version: 2020_03_26_060318) do
+
+  create_table "homes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
