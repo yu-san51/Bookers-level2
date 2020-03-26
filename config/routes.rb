bbffe615f6d65bf6f3a1b38522112c29ad313561
yu-san51 <nekonyan.yuki51@gmail.com>
2020-03-26 06:53:06 +0000

    [Add] header-footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  "/" => "home#index"
+  "/home/about" => "home#show"
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
