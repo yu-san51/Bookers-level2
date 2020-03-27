74c05d6b224604e8efeecc8d732ec5a86eea0957
yu-san51 <nekonyan.yuki51@gmail.com>
2020-03-27 04:29:41 +0000

    [Add] user_edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ApplicationRecord
   end
 
   has_many :books, dependent: :destroy
+  attachment :profile_image
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '61b6ac34e0f64321c4eb731e6c11b9f2d3486a250cea0217d8dbed3c36d104f5c7ddf0ee64f965f388d2571e4ab86c8250b5c363a2249b320b61bca8ad76e95d'
