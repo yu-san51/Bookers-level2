92d40ccd5471856723f81202050e53b920876584
yu-san51 <nekonyan.yuki51@gmail.com>
2020-03-27 06:48:59 +0000

    [Add] user_index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,38 @@
 class BooksController < ApplicationController
 
+
 	def create
-		book = Book.
+		@book = Book.new(book_params)
+		@book.user_id = current_user.id
+		@book.save
+		redirect_to book_path(@book.id)
+
 	end
 
 	def index
 		@books = Book.all
+		@user = User.find(params[:id])
 	end
 
 	def show
 		@user = Book.find(params[:id])
-		@book = @user
+		
 	end
 
 	def edit
 		@book = Book.find(params[:id])
 	end
+
+	def destroy
+		@book = Book.find(params[:id])
+		@book.destroy
+		redirect_to books_path
+	end
+
+
+
+	private
+		def book_params
+			params.require(:book).permit(:title, :body)
+		end
 end
