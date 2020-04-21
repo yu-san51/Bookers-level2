1209aa57e0ad6ceadc01e15e9c76765618227958
yu-san51 <nekonyan.yuki51@gmail.com>
2020-04-21 05:01:53 +0000

    [Add] commentAjax

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,17 +1,16 @@
 class BookCommentsController < ApplicationController
 
 	def create
-	book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to request.referer
+		@book = Book.find(params[:book_id])
+	    comment = current_user.book_comments.new(book_comment_params)
+	    comment.book_id = @book.id
+	    comment.save!
 	end
 
 	def destroy
-		comment = BookComment.find(params[:book_id])
-		comment.destroy
-		redirect_to request.referer
+		comment = BookComment.find(params[:id])
+		@book = comment.book
+		comment.destroy!
 	end
 
 	private
--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,9 @@ Rails.application.routes.draw do
 
   resources :books, only: [:create, :index, :show, :edit, :destroy, :update] do
   	resource :favorites, only: [:create, :destroy]
-  	resource :book_comments, only: [:create, :destroy]
+  	resource :book_comments, only: [:create]
   end
-
+  delete "book_comments/:id", to: "book_comments#destroy", as: "book_comments"
   resources :relationships, only: [:create, :destroy]
   get "/search" => "search#search"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
