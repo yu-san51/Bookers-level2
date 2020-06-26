288617c727f646e55318216684a7af4904234820
yu-san51 <nekonyan.yuki51@gmail.com>
2020-06-26 05:40:12 +0000

    [Add] 評価rate

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,4 @@
 //= require bootstrap-sprockets
 //= require_tree .
 //= require jquery.jpostal
+//= require jquery.raty
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_24_031317) do
+ActiveRecord::Schema.define(version: 2020_06_26_052237) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -26,6 +38,7 @@ ActiveRecord::Schema.define(version: 2020_04_24_031317) do
     t.string "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "rate"
   end
 
   create_table "chats", force: :cascade do |t|
